8600c85eaf046d661c96b673e2b56872197fa04f
jukmmartina <jukmartina@gmail.com>
2023-06-22 20:44:57 +0200

    changed base58 keys

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -136,11 +136,11 @@ public:
         vSeeds.emplace_back("seed.feritcoin.jonasschnelli.ch"); // Jonas Schnelli, only supports x1, x5, x9, and xd
         vSeeds.emplace_back("seed.btc.petertodd.org"); // Peter Todd, only supports x1, x5, x9, and xd
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
-        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
-        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,95);  //95 stand for f (as feritcoin) according to https://en.bitcoin.it/wiki/List_of_address_prefixes
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,122);  //as r
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,33);  //3 as E
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x27, 0x88, 0xB2, 0x1E};
+        base58Prefixes[EXT_SECRET_KEY] = {0x27, 0x88, 0xAD, 0xE4};
 
         bech32_hrp = "bc";
 
